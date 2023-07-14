37ec6172c4e0c41d2b6880ae433eadaa46e3cbc1
franlops <franlopbri@gmail.com>
2023-07-14 16:27:24 -0600

    08 Gestion de usuarios

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 # Imports from Views
 from views.home_views import home_views
 from views.category_views import category_views
+from views.user_views import user_views
 from views.error_views import error_views
 
 app = Flask(__name__)
@@ -10,6 +11,7 @@ app.config['SECRET_KEY'] = 'My Secret Key'
 
 app.register_blueprint(home_views)
 app.register_blueprint(category_views)
+app.register_blueprint(user_views)
 app.register_blueprint(error_views)
 
 if __name__ == '__main__':
